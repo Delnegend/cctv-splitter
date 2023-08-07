bd24da1fc6b8b3e7d95956314999b5a1b68a4032
Delnegend <kiennguyen19323@gmail.com>
2023-08-07 07:44:03 +0700

    refactor(ocr_2_int, main): :recycle: handling edge cases

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 # Usage: python split_video.py <video_file>
 # Description: Split a video into multiple videos based on the difference between specific regions of each frame
 
-import sys
+import argparse
+import multiprocessing
 import os
+import shutil
+import subprocess as sp
+import sys
+
 import cv2
 import numpy as np
-import subprocess as sp
-import argparse
-import shutil
-from pkg import frame_to_timecode, is_similar, OCR_to_int, path
+
+from pkg import OCR_to_int, frame_to_timecode, is_similar, path
+
 
 class COORDS:
-    minute = [[352, 5], [400, 44]] # for differentiating frames + file naming
-    hour = [[290, 5], [340, 44]] # for file naming only
+    minute = [[353, 0], [405, 45]]  # for differentiating frames + file naming
+    hour = [[286, 0], [338, 45]]  # for file naming only
 
-SENSITIVITY = 0.8 # how sensitive the difference between frames (hour/minute region) should be
+
+SENSITIVITY = 0.85  # how sensitive the difference between frames (hour/minute region) should be
+
+ALWAYS_USING_OCR = True  # if True, it will always use OCR to get the minute and hour values, if False, it will use the difference between the frames to get the minute and hour values
+
 
 def read_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="Split a video into multiple videos based on the difference between specific regions of each frame")
+    parser = argparse.ArgumentParser(
+        description="Split a video into multiple videos based on the difference between specific regions of each frame"
+    )
     parser.add_argument("-i", "--input_video", required=True, help="The video file to split")
     parser.add_argument("-s", "--start_frame", default=0, type=int, help="The frame number to start splitting from")
-    parser.add_argument("-e", "--end_frame", default=0, type=int, help="The frame number to stop splitting at, if 0, it will split until the end of the video, if larger than the total number of frames, it will split until the end of the video")
+    parser.add_argument(
+        "-e",
+        "--end_frame",
+        default=0,
+        type=int,
+        help="The frame number to stop splitting at, if 0, it will split until the end of the video, if larger than the total number of frames, it will split until the end of the video",
+    )
     parser.add_argument("-o", "--output_dir", default=".", help="The directory to output the split videos")
     return parser.parse_args()
+
 
 def main(input_video: str, start_frame: int, end_frame: int, output_dir: str):
     # list of frame numbers where the video should be split, this data is just for ffmpeg
@@ -39,10 +56,9 @@ def main(input_video: str, start_frame: int, end_frame: int, output_dir: str):
     framerate = video.get(cv2.CAP_PROP_FPS)
 
     # store the previous frame data for comparison
-    previous_frame_data: dict[str, np.ndarray] = {
-        "minute": np.array([]),
-        "hour": np.array([])
-    }
+    previous_frame_data: dict[str, np.ndarray] = {"minute": np.array([]), "hour": np.array([])}
+
+    previous_actual: dict[str, int] = {"minute": -1, "hour": -1}
 
     # frame_count and total_frames are purely for determining when to stop the loop
     frame_count: int = 0
@@ -60,15 +76,22 @@ def main(input_video: str, start_frame: int, end_frame: int, output_dir: str):
 
             # "crop" the numpy array to get the minute and hour regions and store them as previous frame data
             y1 = COORDS.minute[0][1]
-            y2 = COORDS.minute[1][1]+1
+            y2 = COORDS.minute[1][1] + 1
             x1 = COORDS.minute[0][0]
-            x2 = COORDS.minute[1][0]+1
+            x2 = COORDS.minute[1][0] + 1
             previous_frame_data["minute"] = frame[y1:y2, x1:x2]
+
+            if ALWAYS_USING_OCR:
+                previous_actual["minute"] = OCR_to_int(previous_frame_data["minute"])
+
             y1 = COORDS.hour[0][1]
-            y2 = COORDS.hour[1][1]+1
+            y2 = COORDS.hour[1][1] + 1
             x1 = COORDS.hour[0][0]
-            x2 = COORDS.hour[1][0]+1
+            x2 = COORDS.hour[1][0] + 1
             previous_frame_data["hour"] = frame[y1:y2, x1:x2]
+
+            if ALWAYS_USING_OCR:
+                previous_actual["hour"] = OCR_to_int(previous_frame_data["hour"])
 
             continue
 
@@ -81,26 +104,48 @@ def main(input_video: str, start_frame: int, end_frame: int, output_dir: str):
 
         # "crop" the numpy array to get the minute and hour regions
         y1 = COORDS.minute[0][1]
-        y2 = COORDS.minute[1][1]+1
+        y2 = COORDS.minute[1][1] + 1
         x1 = COORDS.minute[0][0]
-        x2 = COORDS.minute[1][0]+1
+        x2 = COORDS.minute[1][0] + 1
         current_frame_data["minute"] = frame[y1:y2, x1:x2]
         y1 = COORDS.hour[0][1]
-        y2 = COORDS.hour[1][1]+1
+        y2 = COORDS.hour[1][1] + 1
         x1 = COORDS.hour[0][0]
-        x2 = COORDS.hour[1][0]+1
+        x2 = COORDS.hour[1][0] + 1
         current_frame_data["hour"] = frame[y1:y2, x1:x2]
 
+        current_actual: dict[str, int] = {"minute": 0, "hour": 0}
+
         # check if the minute or hour changes
-        minute_changes = not is_similar(current_frame_data["minute"], previous_frame_data["minute"])
-        hour_changes = not is_similar(current_frame_data["hour"], previous_frame_data["hour"])
+        if not ALWAYS_USING_OCR:
+            minute_changes = not is_similar(current_frame_data["minute"], previous_frame_data["minute"])
+            hour_changes = not is_similar(current_frame_data["hour"], previous_frame_data["hour"])
+        else:
+            # current_actual["hour"] = OCR_to_int(current_frame_data["hour"])
+            # hour_changes = current_actual["hour"] != previous_actual["hour"]
+
+            # current_actual["minute"] = OCR_to_int(current_frame_data["minute"])
+            # minute_changes = current_actual["minute"] != previous_actual["minute"]
+
+            # parallelize the OCR
+            with multiprocessing.Pool(2) as p:
+                current_actual["hour"], current_actual["minute"] = p.map(
+                    OCR_to_int, [current_frame_data["hour"], current_frame_data["minute"]]
+                )
+            hour_changes = current_actual["hour"] != previous_actual["hour"]
+            minute_changes = current_actual["minute"] != previous_actual["minute"]
+
         if minute_changes or hour_changes:
             marker_list.append(frame_count)
 
             # <input file name> <actual time> (<start_frame>).mp4
             file_name = os.path.splitext(input_video)[0]
-            actual_hour = OCR_to_int(previous_frame_data["hour"])
-            actual_minute = OCR_to_int(previous_frame_data["minute"])
+            if not ALWAYS_USING_OCR:
+                actual_hour = OCR_to_int(previous_frame_data["hour"])
+                actual_minute = OCR_to_int(previous_frame_data["minute"])
+            else:
+                actual_hour = previous_actual["hour"]
+                actual_minute = previous_actual["minute"]
             start_frame = marker_list[-2]
 
             frame_count_length = len(str(total_frames))
@@ -111,11 +156,32 @@ def main(input_video: str, start_frame: int, end_frame: int, output_dir: str):
 
             # convert frame number to timecode to feed into ffmpeg and run
             begin_time = frame_to_timecode(marker_list[-2], framerate)
-            end_time = frame_to_timecode(marker_list[-1] - 1, framerate) # -1 because we want the frame before the marker
-            sp.run(["ffmpeg", "-i", input_video, "-ss", begin_time, "-to", end_time, "-c:v", "copy", "-c:a", "libopus", output_file], stdout=sp.DEVNULL, stderr=sp.DEVNULL)
+            end_time = frame_to_timecode(marker_list[-1] - 1, framerate)  # -1 because we want the frame before the marker
+            sp.run(
+                [
+                    "ffmpeg",
+                    "-i",
+                    input_video,
+                    "-ss",
+                    begin_time,
+                    "-to",
+                    end_time,
+                    "-c:v",
+                    "copy",
+                    "-c:a",
+                    "libopus",
+                    "-y",
+                    output_file,
+                ],
+                stdout=sp.DEVNULL,
+                stderr=sp.DEVNULL,
+            )
 
         # store the current frame as previous frame
         previous_frame_data = current_frame_data
+        if ALWAYS_USING_OCR:
+            previous_actual = current_actual
+
 
 if __name__ == "__main__":
     not_installed = [app for app in ["ffmpeg", "tesseract"] if shutil.which(app) is None]
--- pkg/OCR_to_int.py
+++ pkg/OCR_to_int.py
@@ -1,6 +1,37 @@
-import pytesseract
+import sys
+
+import cv2
 import numpy as np
+import pytesseract
+
 
 def OCR_to_int(nparray: np.ndarray) -> int:
     """Converts an image to text and returns the integer value"""
-    return int(pytesseract.image_to_string(nparray, config="--psm 7 -c tessedit_char_whitelist=0123456789").strip())
+    # convert to grayscale
+    gray = cv2.cvtColor(nparray, cv2.COLOR_BGR2GRAY)
+
+    # gaussian blur to remove noise
+    blur = cv2.GaussianBlur(gray, (3, 3), 0)
+
+    # sharpen the image
+    kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    sharpen = cv2.filter2D(blur, -1, kernel)
+
+    # upscale the image x4 to make it easier to read using lanczos-3
+    sharpen = cv2.resize(gray, None, fx=4, fy=4, interpolation=cv2.INTER_LANCZOS4)
+
+    result = sharpen
+
+    ocr_result: str = pytesseract.image_to_string(result, config="--psm 7 -c tessedit_char_whitelist=0123456789").strip()
+
+    # save ocr_result to a file with filename as the frame number, overwrite if it already exists
+    # if os.path.exists(f"{ocr_result}.png"):
+    #     os.remove(f"{ocr_result}.png")
+    # cv2.imwrite(f"{ocr_result}.png", result)
+
+    # print("OCR result:", ocr_result)
+    if ocr_result == "":
+        print("OCR failed to read the image")
+        cv2.imwrite("error.png", result)
+        sys.exit(1)
+    return int(ocr_result)
